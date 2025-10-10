87edd0a5aa4d07b233340b673bd0f2fcdffae38e
Christian Kim <chris.kim320@gmail.com>
2025-10-10 13:11:01 -0700

    create function to get event ids

--- server/controllers/nflbets.controllers.js
+++ server/controllers/nflbets.controllers.js
@@ -18,14 +18,54 @@ function getEndDateISO() {
 export const getNFLTeamLines = async (req, res) => {
     const start_date = getTodaysDateISO();
     const end_date = getEndDateISO();
-    console.log(start_date, end_date);
     try {
         const response = await axios.get(`https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/`, {
             params: {
                 apiKey: process.env.ODDS_API_KEY,
                 regions: 'us,us2',
                 markets: 'h2h,spreads,totals',
-                bookmakers: 'draftkings,fanduel,betmgm,betus,fanatics,espnbet',
+                bookmakers: 'draftkings,fanduel,betmgm,betus,espnbet',
+                oddsFormat: 'american',
+                dateFormat: 'iso',
+                commenceTimeFrom: start_date,
+                commenceTimeTo: end_date
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching team lines:', error);
+        res.status(500).json({ error: 'Failed to fetch team lines' });
+    }
+};
+
+async function getNFLEventIds() {
+    const start_date = getTodaysDateISO();
+    const end_date = getEndDateISO();
+    try {
+        const resopnse = await axios.get(`https://api.the-odds-api.com/v4/sports/americanfootball_nfl/events?${process.env.ODDS_API_KEY}`, {
+            params: {
+                dateFormat: 'iso',
+                commenceTimeFrom: start_date,
+                commenceTimeTo: end_date
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching event Ids:', error);
+        res.status(500).json({ error: 'Failed to fetch team lines '});
+    }
+};
+
+export const getNFLPlayerProps = async (req, res) => {
+    const start_date = getTodaysDateISO();
+    const end_date = getEndDateISO();
+    const data = getNFLEventIds();
+    try {
+        const response = await axios.get(`https://api.the-odds-api.com/v4/sports/americanfootball_nfl/events/`, {
+            params: {
+                apiKey: process.env.ODDS_API_KEY,
+                regions: 'us,us2,us_dfs',
+                markets:'player_pass_yds,player_rush_yds,player_reception_yds',
+                bookmakers: 'draftkings,fanduel,betmgm,betus,espnbet,prizepicks,underdog',
                 oddsFormat: 'american',
                 dateFormat: 'iso',
                 commenceTimeFrom: start_date,
--- server/routes/nflbets.routes.js
+++ server/routes/nflbets.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getNFLTeamLines } from '../controllers/nflbets.controllers.js';
+import { getNFLTeamLines, getNFLPlayerProps } from '../controllers/nflbets.controllers.js';
 
 const router = express.Router();
 
 router.get('/', getNFLTeamLines);
+
+router.get('/playerprops', getNFLPlayerProps);
 
 export default router;
