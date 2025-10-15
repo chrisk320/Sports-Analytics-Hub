fe08f6e671cce3c1634825c1b569247d3c015120
Christian Kim <chris.kim320@gmail.com>
2025-10-15 14:01:44 -0700

    add nfl game modal

---
+++ frontend/src/components/NFLGameModal.jsx
@@ -0,0 +1,7 @@
+const NFLGameModal = () => {
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center p-4 z-50">
+            
+        </div>
+    );
+}
