a54445ecf7c03fa3adc9226cfbf021502cfb3473
Christian Kim <chris.kim320@gmail.com>
2025-11-17 19:58:15 -0800

    add server.js debug

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ dotenv.config()
 
 const app = express();
 
-// Configure CORS for Azure deployment
 const corsOptions = {
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
@@ -19,6 +18,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('NBA Stats API is running');
+});
 app.use("/players", stats);
 app.use('/users', userFavorites);
 app.use('/teams', teams);
